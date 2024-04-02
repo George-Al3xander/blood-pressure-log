726d10e8ab7fcda853ee77b82178fc2d7adcf2a9
George <george.valuiskyi@gmail.com>
2024-04-02 11:36:10 +0300

    refactor: remove /reports/count api endpoint
    Remove /reports/count api endpoint due to the inefficiency.

--- src/app/api/mongo/reports/route.ts
+++ src/app/api/mongo/reports/route.ts
@@ -1,84 +1,103 @@
-import { NextRequest, NextResponse } from "next/server";
-import { TReportData } from "../../../../../lib/auth/zodSchemas";
-import { connectToDatabase } from "../../../../../lib/mongo";
+import { NextRequest, NextResponse } from "next/server"
+import { TReportData } from "../../../../../lib/auth/zodSchemas"
+import { connectToDatabase } from "../../../../../lib/mongo"
 import Report from "../../../../../lib/mongo/schemas/report"
 
 import jwt from "jsonwebtoken"
-import User from "../../../../../lib/mongo/schemas/user";
-import { getSession } from "../../../../../lib/auth/actions";
-import { headers } from "next/headers";
+import User from "../../../../../lib/mongo/schemas/user"
+import { getSession } from "../../../../../lib/auth/actions"
+import { headers } from "next/headers"
+import dayjs from "dayjs"
 
+export async function GET(req: NextRequest) {
+  console.log("reports fetched")
+  const headersList = headers()
+  const authToken = (headersList.get("authorization") || "")
+    .split("Bearer ")
+    .at(1)
+  //const id  = req.nextUrl.searchParams.get("id");
+  try {
+    if (!authToken) throw new Error("Invalid token")
 
-export async function GET(req: NextRequest) {   
-    console.log("reports fetched") 
-    const headersList = headers();
-    const authToken = (headersList.get("authorization") || '').split("Bearer ").at(1);
-    //const id  = req.nextUrl.searchParams.get("id");
-    try {   
-      
-        if(!authToken) throw new Error("Invalid token")
+    const { email, userId } = jwt.verify(
+      authToken!,
+      process.env.JWT_SECRET_KEY!
+    ) as { userId?: string; email?: string }
 
-        const {email,userId} = jwt.verify(authToken!, process.env.JWT_SECRET_KEY!) as {userId?:string,email?:string};
-               
-        if(!email) throw new Error("Invalid user info");
+    if (!email) throw new Error("Invalid user info")
 
-        await connectToDatabase();
-        const user =  await User.findOne({email}) ;
-        if(!user) throw new Error("Invalid user info");
-        const page = Number(req.nextUrl.searchParams.get("page") || "0");
-        const pageSize = Number(req.nextUrl.searchParams.get("pageSize") || "30");
-        const skip = page * pageSize
-        const reports = await Report.find({userId}).limit(pageSize).skip(skip);
-        //const count = await Report.countDocuments({userId})
-      
-        return NextResponse.json({status: 200,success: true,reports})     
-
-    } catch (error) {      
-       const message = 
-       typeof error == "string" 
-       ? error 
-       : 
-       (typeof error == "object")
-       ? "message" in error!  
-       ? error!.message 
-       : "Something went wrong"
-       : "Something went wrong"
-       return NextResponse.json({status: 500,message})        
-    }   
-}
-
-export async function PUT() {   
-    // const headersList = headers();
-    // const authToken = (headersList.get("authorization") || '').split("Bearer ").at(1);
-  
-    //     if(!authToken) throw new Error("Invalid token");
-    //     const {email} = jwt.verify(authToken!, process.env.JWT_SECRET_KEY!) as {userId?:string,email?:string};
-               
-    //     if(!email) throw new Error("Invalid user info");
-        
-     
-      
-       
-    //  await connectToDatabase();
-    throw new Error("BRUH")
-    return NextResponse.json({success: true})
+    await connectToDatabase()
+    const user = await User.findOne({ email })
+    if (!user) throw new Error("Invalid user info")
+    const page = Number(req.nextUrl.searchParams.get("page") || "0")
+    const pageSize = Number(req.nextUrl.searchParams.get("pageSize") || "30")
+    const gte = req.nextUrl.searchParams.get("gte")
+    const lte = req.nextUrl.searchParams.get("lte") 
+    const skip = page * pageSize
     
+    const searchModel: {userId?:string,date?: any} = {
+        userId,
+        date: {
+          $gte: dayjs(gte ).startOf("day").toDate(),
+          $lte: dayjs(lte).endOf("day").toDate(),
+        },
+      }
+
+     if(!lte || !gte) {
+        delete searchModel.date
+     } 
+    const reports = await Report.find(searchModel)
+      .limit(pageSize)
+      .skip(skip)
+
+    const count = await Report.countDocuments({ userId })
+    return NextResponse.json({ status: 200, success: true, reports, count })
+  } catch (error) {
+    const message =
+      typeof error == "string"
+        ? error
+        : typeof error == "object"
+        ? "message" in error!
+          ? error!.message
+          : "Something went wrong"
+        : "Something went wrong"
+    return NextResponse.json({ status: 500, message })
+  }
 }
 
+export async function PUT() {
+  // const headersList = headers();
+  // const authToken = (headersList.get("authorization") || '').split("Bearer ").at(1);
 
-export async function POST(req: NextRequest){
-    const {date,sys,dia,pulse,rating,notes}: TReportData = await req.json();
-    const {id} = await getSession();
-    try {
-      
-        await connectToDatabase(); 
-        const newReport = new Report({date,sys,dia,pulse,rating,notes,userId: id});
-        await newReport.save();
-        
-        return NextResponse.json({status: 200,success: true})        
+  //     if(!authToken) throw new Error("Invalid token");
+  //     const {email} = jwt.verify(authToken!, process.env.JWT_SECRET_KEY!) as {userId?:string,email?:string};
 
-    } catch (error) {
-       /// console.log(error)
-       return NextResponse.json({status: 500,message: error})        
-    } 
+  //     if(!email) throw new Error("Invalid user info");
+
+  //  await connectToDatabase();
+  //throw new Error("BRUH")
+  return NextResponse.json({ success: true })
+}
+
+export async function POST(req: NextRequest) {
+  const { date, sys, dia, pulse, rating, notes }: TReportData = await req.json()
+  const { id } = await getSession()
+  try {
+    await connectToDatabase()
+    const newReport = new Report({
+      date,
+      sys,
+      dia,
+      pulse,
+      rating,
+      notes,
+      userId: id,
+    })
+    await newReport.save()
+
+    return NextResponse.json({ status: 200, success: true })
+  } catch (error) {
+    /// console.log(error)
+    return NextResponse.json({ status: 500, message: error })
+  }
 }
