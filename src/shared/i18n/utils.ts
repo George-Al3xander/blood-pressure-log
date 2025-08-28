1c9b12c56154583cbd09a8bd3780dba5f3630cd8
George <george.valuiskyi@gmail.com>
2025-08-28 14:30:15 +0300

    fix: set-user-locale
    Remove the implicit return from setUserLocale to fix a runtime error
    caused by returning the result of an async cookie operation.

--- src/shared/i18n/utils.ts
+++ src/shared/i18n/utils.ts
@@ -1,12 +1,13 @@
 "use server";
 
-import { routing } from "@/shared/i18n/routing";
 import { cookies } from "next/headers";
+import { routing } from "./routing";
 
 const COOKIE_NAME = "NEXT_LOCALE";
 
 export const getUserLocale = async () =>
     (await cookies()).get(COOKIE_NAME)?.value || routing.defaultLocale;
 
-export const setUserLocale = async (locale: string) =>
+export const setUserLocale = async (locale: string) => {
     (await cookies()).set(COOKIE_NAME, locale);
+};
