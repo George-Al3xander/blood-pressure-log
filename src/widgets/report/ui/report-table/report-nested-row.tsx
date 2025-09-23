c8c4cf6c687dec3509c027124e55a234e4fa6eff
George <george.valuiskyi@gmail.com>
2025-09-23 15:44:32 +0300

    feat: report-nested-row
    Introduce ReportNestedRow component to render collapsible table rows
    with detailed field-value pairs from TReport. Utilizes
    ReportTableCell for consistent rendering of labels and data.
    Supports optional title and accessibility via ARIA labels.

---
+++ src/widgets/report/ui/report-table/report-nested-row.tsx
@@ -0,0 +1,72 @@
+import { ReportTableCell } from "@/entities/report";
+import { TReport } from "@/shared/model";
+import {
+    Collapse,
+    Table,
+    TableBody,
+    TableCell,
+    TableRow,
+    Typography,
+} from "@mui/material";
+import Box from "@mui/material/Box";
+import { FC } from "react";
+
+type Props = {
+    open: boolean;
+    title?: string;
+    data: {
+        field: keyof TReport;
+        value: TReport[keyof TReport];
+    }[];
+};
+
+export const ReportNestedRow: FC<Props> = ({ data, open, title }) => {
+    return (
+        <TableRow>
+            <TableCell sx={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                    <Box sx={{ margin: 1 }} component="section">
+                        {title && (
+                            <Typography
+                                variant="h6"
+                                gutterBottom
+                                component="div"
+                            >
+                                {title}
+                            </Typography>
+                        )}
+                        <Table size="small" aria-label={title}>
+                            <TableBody>
+                                {data.map((row) => (
+                                    <TableRow
+                                        key={`inner-table-row-${row.field}`}
+                                        sx={{
+                                            "&:last-child td, &:last-child th":
+                                                {
+                                                    borderBottom: "none",
+                                                },
+                                        }}
+                                    >
+                                        <ReportTableCell
+                                            displayVariant="label"
+                                            component="th"
+                                            scope="row"
+                                            sx={{
+                                                fontWeight: 700,
+                                            }}
+                                            {...row}
+                                        />
+                                        <ReportTableCell
+                                            displayVariant="data"
+                                            {...row}
+                                        />
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </Box>
+                </Collapse>
+            </TableCell>
+        </TableRow>
+    );
+};
