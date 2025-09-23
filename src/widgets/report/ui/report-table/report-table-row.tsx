41fcffcc889c14f512dd11dd1e1dc22765c4b827
George <george.valuiskyi@gmail.com>
2025-09-23 16:01:24 +0300

    feat: report-table-row
    Implement expandable rows in the report table to display nested data.
    Add toggle button with icons to show or hide detailed report info.
    Update localization files with Ukrainian & English translations for report table
    labels and expand button texts to support internationalization.

---
+++ src/widgets/report/ui/report-table/report-table-row.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { ReportTableCell, ReportTableConfig } from "@/entities/report";
+import { TReport } from "@/shared/model";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import { IconButton, TableCell } from "@mui/material";
+import TableRow from "@mui/material/TableRow";
+import { useTranslations } from "next-intl";
+import { FC, useState } from "react";
+import { ReportNestedRow } from "./report-nested-row";
+
+type Props = ReportTableConfig<{ report: TReport }>;
+
+export const ReportTableRow: FC<Props> = ({
+    report,
+    mainColumns,
+    nestedColumns = [],
+}) => {
+    const t = useTranslations("reportTable");
+
+    const [open, setOpen] = useState(false);
+    const toggleCollapse = () => setOpen((prev) => !prev);
+
+    const hasNestedColumns = nestedColumns.length > 0;
+
+    return (
+        <>
+            <TableRow>
+                {hasNestedColumns && (
+                    <TableCell align="left" width={2}>
+                        <IconButton
+                            size="small"
+                            aria-label={t(
+                                `expandButton.${open ? "hide" : "show"}`,
+                            )}
+                            onClick={toggleCollapse}
+                        >
+                            {open ? (
+                                <KeyboardArrowUpIcon />
+                            ) : (
+                                <KeyboardArrowDownIcon />
+                            )}
+                        </IconButton>
+                    </TableCell>
+                )}
+                {mainColumns.map((field) => (
+                    <ReportTableCell
+                        value={report[field]}
+                        field={field}
+                        displayVariant="data"
+                        key={`table-cell-${field}`}
+                    />
+                ))}
+            </TableRow>
+            {hasNestedColumns && (
+                <ReportNestedRow
+                    title={t("nestedTableTitle")}
+                    open={open}
+                    data={nestedColumns.map((k) => ({
+                        field: k,
+                        value: report[k],
+                    }))}
+                />
+            )}
+        </>
+    );
+};
