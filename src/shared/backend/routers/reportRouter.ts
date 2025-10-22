b88f88b520424455c1572b5a37950a28d256607e
George <george.valuiskyi@gmail.com>
2025-10-22 11:23:29 +0300

    feat: reports pagination
    Implement pagination for the list query in the report router. Add page
    and perPage parameters, allowing users to fetch reports in a paginated
    manner. Introduce a count query to return the total number of reports
    and the number of pages based on the perPage value. This improves
    performance and scalability for large report datasets.

--- src/shared/backend/routers/reportRouter.ts
+++ src/shared/backend/routers/reportRouter.ts
@@ -1,15 +1,56 @@
 import { reportModel, reportSchema, TReport } from "@/shared/model";
 import { getTranslations } from "next-intl/server";
+import { z } from "zod";
 import { baseProcedure, createTRPCRouter } from "../init";
 
-export const reportRouter = createTRPCRouter({
-    list: baseProcedure.query(async ({ ctx: { auth } }) => {
-        const reports = await reportModel.find({ userId: auth.userId });
+const PAGE_NUMBER = 1;
+const PER_PAGE = 20;
 
-        return {
-            reports,
-        };
-    }),
+const paginationModel = z
+    .object({
+        page: z.number().optional().default(PAGE_NUMBER),
+        perPage: z.number().optional().default(PER_PAGE),
+    })
+    .default({
+        page: PAGE_NUMBER,
+        perPage: PER_PAGE,
+    });
+
+export const reportRouter = createTRPCRouter({
+    list: baseProcedure.input(paginationModel).query(
+        async ({
+            input: { page, perPage },
+            ctx: {
+                auth: { userId },
+            },
+        }) => {
+            const skip = (page - 1) * perPage;
+
+            const reports = (await reportModel
+                .find({ userId })
+                .sort({ date: "desc" })
+                .limit(perPage)
+                .skip(skip)) as unknown as Report[];
+
+            return {
+                reports,
+            };
+        },
+    ),
+    count: baseProcedure.input(paginationModel).query(
+        async ({
+            input: { perPage },
+            ctx: {
+                auth: { userId },
+            },
+        }) => {
+            const count = await reportModel.countDocuments({ userId });
+
+            const pages = Math.ceil(count / perPage);
+
+            return { count, pages };
+        },
+    ),
     create: baseProcedure
         .input(async (data): Promise<TReport> => {
             const t = await getTranslations("validation");
